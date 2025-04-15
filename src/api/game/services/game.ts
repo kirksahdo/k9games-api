c863ec550a6b786e30c87f32f29a6cf438a2ab8a
Kirk <ksahdo10@gmail.com>
2025-04-15 15:18:54 -0400

    feat: add expcetions treatment

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -12,70 +12,89 @@ const GAME_SERVICE = "api::game.game";
 
 type Rating = "BR0" | "BR10" | "BR12" | "BR14" | "BR16" | "BR18";
 
-function isRating(value: string): value is Rating {
-  return ["BR0", "BR10", "BR12", "BR14", "BR16", "BR18"].includes(value);
-}
-
-async function getGameInfo(slug: string) {
-  const gogUrl = `https://www.gog.com/en/game/${slug}`;
-  const body = await axios.get(gogUrl);
-  const dom = new JSDOM(body.data);
-
-  const rawDescription = dom.window.document.querySelector(".description");
-  const description = rawDescription.innerHTML;
-  const shortDescription = rawDescription.textContent.slice(0, 160);
-
-  const ratingElement = dom.window.document.querySelector(
-    ".age-restrictions__icon use",
-  );
-
-  const rating = ratingElement
-    ? ratingElement.getAttribute("xlink:href").replace("#", "").replace("_", "")
-    : "BR0";
-
-  let validatedRating: Rating = "BR0";
-
-  if (isRating(rating)) {
-    validatedRating = rating;
-  }
-
-  return {
-    description,
-    shortDescription,
-    rating: validatedRating,
-  };
-}
-
 type EntityService =
   | "api::developer.developer"
   | "api::publisher.publisher"
   | "api::category.category"
   | "api::platform.platform";
 
-async function getByName(name: string, entityService: EntityService) {
-  const item = await strapi
-    .documents(entityService)
-    .findMany({ filters: { name } });
+function Exception(e) {
+  return { e, data: e.data && e.data.errors && e.data.errors };
+}
 
-  return item.length > 0 ? item[0] : null;
+function isRating(value: string): value is Rating {
+  return ["BR0", "BR10", "BR12", "BR14", "BR16", "BR18"].includes(value);
+}
+
+async function getGameInfo(slug: string) {
+  try {
+    const gogUrl = `https://www.gog.com/en/game/${slug}`;
+    const body = await axios.get(gogUrl);
+    const dom = new JSDOM(body.data);
+
+    const rawDescription = dom.window.document.querySelector(".description");
+    const description = rawDescription.innerHTML;
+    const shortDescription = rawDescription.textContent.slice(0, 160);
+
+    const ratingElement = dom.window.document.querySelector(
+      ".age-restrictions__icon use",
+    );
+
+    const rating = ratingElement
+      ? ratingElement
+          .getAttribute("xlink:href")
+          .replace("#", "")
+          .replace("_", "")
+      : "BR0";
+
+    let validatedRating: Rating = "BR0";
+
+    if (isRating(rating)) {
+      validatedRating = rating;
+    }
+
+    return {
+      description,
+      shortDescription,
+      rating: validatedRating,
+    };
+  } catch (error) {
+    console.error("getGameInfo: ", Exception(error));
+  }
+}
+
+async function getByName(name: string, entityService: EntityService) {
+  try {
+    const item = await strapi
+      .documents(entityService)
+      .findMany({ filters: { name } });
+
+    return item.length > 0 ? item[0] : null;
+  } catch (error) {
+    console.error("getByName: ", Exception(error));
+  }
 }
 
 async function create(name: string, entityService: EntityService) {
-  const slug = slugify(name, { lower: true, strict: true });
-  const item = await getByName(name, entityService);
+  try {
+    const slug = slugify(name, { lower: true, strict: true });
+    const item = await getByName(name, entityService);
 
-  if (!item) {
-    console.log(`Creating ${entityService} ${name} ${slug}`);
-    const result = await strapi.documents(entityService).create({
-      data: {
-        name,
-        slug,
-      },
-    });
-    return result;
+    if (!item) {
+      console.log(`Creating ${entityService} ${name} ${slug}`);
+      const result = await strapi.documents(entityService).create({
+        data: {
+          name,
+          slug,
+        },
+      });
+      return result;
+    }
+
+    return item;
+  } catch (error) {
+    console.error("create: ", Exception(error));
   }
-
-  return item;
 }
 
 async function createGameSubfields(game) {
@@ -117,14 +136,18 @@ async function sendImageToApi({ image, game, field = "cover" }) {
 
   console.info(`Uploading ${field} image: ${game.slug}.jpg`);
 
-  await axios({
-    method: "POST",
-    url: `http://localhost:1337/api/upload/`,
-    data: formData,
-    headers: {
-      "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
-    },
-  });
+  try {
+    await axios({
+      method: "POST",
+      url: `http://${process.env.HOST}:${process.env.PORT}/api/upload/`,
+      data: formData,
+      headers: {
+        "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+      },
+    });
+  } catch (error) {
+    console.error("sendImageToApi: ", Exception(error));
+  }
 }
 
 async function uploadGamePhotos(game, createdGame) {
@@ -168,6 +191,7 @@ async function createGame(game) {
         platforms: platforms,
       },
     });
+
     uploadGamePhotos(game, createdGame);
   } catch (error) {
     console.error("Error creating game:", game.title, error);
@@ -176,14 +200,18 @@ async function createGame(game) {
 
 export default factories.createCoreService("api::game.game", () => ({
   populate: async (params) => {
-    const gogApiUrl = "https://catalog.gog.com/v1/catalog?order=desc&page=1";
+    try {
+      const gogApiUrl = "https://catalog.gog.com/v1/catalog?order=desc&page=1";
 
-    const {
-      data: { products },
-    } = await axios.get(gogApiUrl);
+      const {
+        data: { products },
+      } = await axios.get(gogApiUrl);
 
-    for (const game of products) {
-      await createGame(game);
+      for (const game of products) {
+        await createGame(game);
+      }
+    } catch (error) {
+      console.error("populate: ", Exception(error));
     }
   },
 }));
